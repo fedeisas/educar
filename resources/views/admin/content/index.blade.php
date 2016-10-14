b2f575881b3efb95d5f6895ca1d272df34f15ff9
Fede Isas <federicoisas@gmail.com>
2016-10-14 17:18:34 -0300

    Buttons width

--- resources/views/admin/content/index.blade.php
+++ resources/views/admin/content/index.blade.php
@@ -32,26 +32,28 @@
                                 <td>{{ $item->title }}</td>
                                 <td>{{ $item->created_at->diffForHumans() }}</td>
                                 <td class="text-right">
-                                    <a href="{{ url('/content/' . $item->id) }}" class="btn btn-success btn-xs" title="Ver">
-                                        <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
-                                        Ver
-                                    </a>
-                                    <a href="{{ url('/admin/content/' . $item->id . '/edit') }}" class="btn btn-primary btn-xs" title="Editar">
-                                        <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-                                        Editar
-                                    </a>
-                                    {!! Form::open([
-                                        'method' => 'DELETE',
-                                        'url' => ['/admin/content', $item->id],
-                                        'style' => 'display:inline'
-                                    ]) !!}
-                                        {!! Form::button('<span class="glyphicon glyphicon-trash" aria-hidden="true" title="Eliminar"></span>Eliminar', array(
-                                                'type' => 'submit',
-                                                'class' => 'btn btn-danger btn-xs',
-                                                'title' => 'Borrar',
-                                                'onclick'=>'return confirm("Está seguro que quiere borrar este contenido?")'
-                                        )) !!}
-                                    {!! Form::close() !!}
+                                    <div style="min-width: 200px;">
+                                        <a href="{{ url('/content/' . $item->id) }}" class="btn btn-success btn-xs" title="Ver">
+                                            <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+                                            Ver
+                                        </a>
+                                        <a href="{{ url('/admin/content/' . $item->id . '/edit') }}" class="btn btn-primary btn-xs" title="Editar">
+                                            <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                                            Editar
+                                        </a>
+                                        {!! Form::open([
+                                            'method' => 'DELETE',
+                                            'url' => ['/admin/content', $item->id],
+                                            'style' => 'display:inline'
+                                        ]) !!}
+                                            {!! Form::button('<span class="glyphicon glyphicon-trash" aria-hidden="true" title="Eliminar"></span>Eliminar', array(
+                                                    'type' => 'submit',
+                                                    'class' => 'btn btn-danger btn-xs',
+                                                    'title' => 'Borrar',
+                                                    'onclick'=>'return confirm("Está seguro que quiere borrar este contenido?")'
+                                            )) !!}
+                                        {!! Form::close() !!}
+                                    </div>
                                 </td>
                             </tr>
                         @endforeach
